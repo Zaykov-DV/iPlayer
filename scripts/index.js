4dd21d8d74aae057259bcc4fb5a87feeeb84b25e
Дмитрий <grunger.ekb@gmail.com>
2020-07-29 14:36:04 +0500

    День 2. Видеоплеер

--- scripts/index.js
+++ scripts/index.js
@@ -2,24 +2,26 @@ import {videoPlayerInit} from "./videoPlayer.js";
 import {musicPlayerInit} from "./musicPlayer.js";
 import {radioPlayerInit} from "./radioPlayer.js";
 
-/*
-videoPlayerInit();
+
+
 musicPlayerInit();
 radioPlayerInit();
-*/
+
 
 const playerBtn = document.querySelectorAll('.player-btn');
 const playerBlock = document.querySelectorAll('.player-block');
-const temp = document.querySelector('.temp')
+const temp = document.querySelector('.temp');
 
 const deactivationPlayer = () => {
-    temp.style.display = 'none'
+    temp.style.display = 'none';
     playerBlock.forEach((item) => item.classList.remove('active'));
     playerBtn.forEach((item) => item.classList.remove('active'));
 };
 
 playerBtn.forEach((btn, i) => btn.addEventListener('click', () => {
-      deactivationPlayer()
+      deactivationPlayer();
       btn.classList.add('active');
-      playerBlock[i].classList.add('active')
-    }))
+      playerBlock[i].classList.add('active');
+    }));
+
+videoPlayerInit();
